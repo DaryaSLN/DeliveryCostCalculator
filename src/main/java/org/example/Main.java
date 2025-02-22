db6e3b647d57144232e9ce0554e89cc1c85b3483
Darya <darya.slinkova@gmail.com>
2025-02-22 19:36:19 +0300

    Delete extra info

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        DeliveryCalculator deliveryCalculator = new DeliveryCalculator(40.0,
-                null, false, DeliveryServiceLoad.VERY_HIGH);
-        System.out.println(deliveryCalculator);
-        // добавить проверки на null, отрицательные, 0
-        // обработать максимальные значения
-        // вынести в отдельные методы расчёты по каждому параметру.
+
     }
 }
